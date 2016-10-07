1cf95dc48294353e903dd3c7ff3e0193d745bae7
Marc-André Gagnon <marc-andre.gagnon@cyberjustice.ca>
2016-10-07 15:46:16 -0400

    Oubli d'un ;

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.Write("1\n2\n3\n4")
+            Console.Write("1\n2\n3\n4");
             Console.ReadKey();
         }
     }
